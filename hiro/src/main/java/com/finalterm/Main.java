367aa25248dfb5aa917baf59b692b0ccef44e235
DESKTOP-EE7A85G\ACER <christianmiranda0815@gmail.com>
2024-10-10 09:57:34 +0800

    Nakajima

--- hiro/src/main/java/com/finalterm/Main.java
+++ hiro/src/main/java/com/finalterm/Main.java
@@ -2,6 +2,8 @@ package com.finalterm;
 
 public class Main {
     public static void main(String[] args) {
+        int a = 9;
         System.out.println("Hello world!");
+        System.out.println(a);
     }
 }
---
+++ hiro/src/main/java/com/finalterm/NakajimaAct.java
@@ -0,0 +1,7 @@
+package com.finalterm;
+
+public class NakajimaAct {
+    public static void main(String[] args) {
+        System.err.println(999);
+    }
+}
